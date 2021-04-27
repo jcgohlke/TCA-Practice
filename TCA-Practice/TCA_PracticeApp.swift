74e18127681849c1d26b62af1fee4519cc24a401
Ben Gohlke <jcgohlke@me.com>
2021-04-26 20:18:25 -0400

    Initial Commit

---
+++ TCA-Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA-Practice
+//
+//  Created by Joben Gohlke on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
